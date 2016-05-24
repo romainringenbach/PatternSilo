812f4b1a104bf77307f5b9ea1e1f5d62582108b3
nealith <nealith@neaworld.fr>
2016-05-24 23:03:08 +0200

    Begin the DBObject

--- PatternSiloDBObject.js
+++ PatternSiloDBObject.js
@@ -1,3 +1,113 @@
+var DBObject = function(socket,user,ready){
+
+/*	==========================================================================
+ 	MYSQL CONFIGURATION
+	========================================================================== */
+
+	this.mysql = require('mysql');
+
+	this.fs = require('fs')
+
+	this.configuration = JSON.parse(fs.readFileSync('mysql_conf.json', 'utf8'););
+
+	this.connection = mysql.createConnection(configuration);
+
+/*	==========================================================================
+	GLOBAL ARGUMENTS
+	========================================================================== */
+
+	this.patternsList = new Array();
+	this.currentPattern = null;	
+	this.schema = null;
+
+	this.queries = {
+
+		getAllFrom : 'SELECT * FROM ??',
+		getAllFromWhere : 'SELECT * FROM ?? WHERE ?? = ?'
+
+	}	
+
+	/// Récupérer l'ID
+
+	/// Récupérer le schema
+
+	/// Mettre un status : ready / notReady
+
+	/// Récupérer l'intégralité des données et la renvoyée sur un canal prédéfinie ?
+
+	/// 
+};
+
+DBObject.prototype.emitOnSocket = function(canal,data) {
+	this.socket.emit(canal,data);
+};
+
+DBObject.prototype.emitMessageOnSocket = function(message,type) {
+	this.emitOnSocket('message',{message:message,type:type});
+};
+
+DBObject.prototype.pushQuery = function(query,callback){
+	connection.query(query, callback)
+};
+
+DBObject.prototype.run = function(query) {
+
+
+
+};
+
+DBObject.prototype.readDB = function(first_argument) {
+	this.getPatternsList();
+
+	for each(pattern in this.patternsList){
+		this.currentPattern = pattern;
+		pattern.characteristics = new Array();
+		this.getPatternCharac(pattern.id);
+	}
+};
+
+DBObject.prototype.getPatternsList = function() {
+	var queryStructure = this.queries.getAllFrom;
+	var queryValues = ['Patterns'];
+	var query = mysql.format(queryStructure,queryValues);	
+	pushQuery(query,this.getPatternsListCallBack);
+};
+DBObject.prototype.getPatternsListCallBack = function(err, rows, fields) {
+	this.patternsList = rows;
+	if (!err) {
+		this.patternsList = rows;
+	} else {
+		console.log(err);
+	}	
+};
+DBObject.prototype.getPatternCharac = function(patternId) {
+	var queryStructure = this.queries.getAllFromWhere;
+	var queryValues = ['PatternsCharacteristics','id',patternId];
+	var query = mysql.format(queryStructure,queryValues);
+	pushQuery(query,this.getPatternCharacCallBack);	
+};
+DBObject.prototype.getPatternCharacCallBack = function(err, rows, fields) {
+	if (!err) {
+		for each(row in rows){
+			this.currentPattern.characteristics.push(row);
+		}
+	} else {
+		console.log(err);
+	}
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*	==========================================================================
  	BASIC MYSQL CONFIGURATIONS
 	========================================================================== */
@@ -30,17 +140,6 @@ var queryPool = {
 /*	==========================================================================
 	OBJECTS
 	========================================================================== */
-
-var DBObject = function(){
-
-
-
-
-
-
-
-}
-
 var DBReader = function(){
 
 	var patternsList = new Array();
